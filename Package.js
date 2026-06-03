ac830253e403d97fc5e477615e7e75ea64931b25
Sam42213 <notsam2009@gmail.com>
2026-06-03 15:51:14 +0530

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,12 @@
+{
+  "name": "aternos-247-bot",
+  "version": "1.0.0",
+  "description": "A Mineflayer bot to keep Aternos servers online 24/7",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "mineflayer": "^4.21.0"
+  }
+}
